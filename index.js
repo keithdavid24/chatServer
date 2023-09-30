49f63c93d0c4c73802076e20f5ec9e94915d5f98
Keith David <kdavid8@student.cccs.edu>
2023-09-30 15:26:09 -0600

    ticket one

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+const {PORT, MONGO} =process.env;
+
+
+mongoose.connect(`${MONGO}/chatServer`);
+
+const db = mongoose.connection;
+
+db.once('open', () => console.log(`Connected to: ${MONGO}`))
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
+
